c0898715adcb67dc182d2e078bc4e184851162ea
CaelumNuntium <st087042@student.spbu.ru>
2022-11-03 01:05:04 +0300

    Add example text

--- line_wrapping.py
+++ line_wrapping.py
@@ -1,2 +1,16 @@
 import textwrap
 import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("input", action="store", help="Name of the input file")
+parser.add_argument("-o", "--output", action="store", default="output.txt", help="Name of the output file")
+parser.add_argument("--encoding", action="store", default="utf_8", help="Input file encoding. Default value is 'utf_8'")
+args = parser.parse_args()
+with open(args.input, "r", encoding=args.encoding) as inp:
+    with open(args.output, "w") as out:
+        for s in inp:
+            if len(s) > 90:
+                strings = textwrap.wrap(s, width=70)
+                for sw in strings:
+                    out.write(sw + "\n")
+            else:
+                out.write(s + "\n")
